bd9692808a8d0e14da11036f26ef4b162e0e6aa0
Michael Laier <m_laie01@uni-muenster.de>
2015-08-10 18:42:50 +0200

    [domaindescriptions.basic] add PolygonalDomain and PieDomain

--- src/pymor/domaindescriptions/basic.py
+++ src/pymor/domaindescriptions/basic.py
@@ -260,3 +260,71 @@ class CircleDomain(DomainDescriptionInterface):
 
     def __repr__(self):
         return 'CircleDomain({})'.format(self.domain)
+
+
+class PolygonalDomain(DomainDescriptionInterface):
+    point_types = {'Point', 'Physical Point'}
+    line_types = {'BSpline', 'Circle', 'CatmullRom', 'Ellipse', 'Line', 'Spline', 'Line Loop', 'Compound Line',
+                           'Physical Line'}
+    surface_types = {'Plane Surface', 'Ruled Surface', 'Surface Loop', 'Compound Surface', 'Physical Surface'}
+
+    def __init__(self, points, lines, surfaces, boundary_types, geo_file_path):
+        self.geo_file_path = geo_file_path
+        f = open(geo_file_path, 'w')
+
+        for point_type, ps in points.iteritems():
+            assert point_type in self.point_types
+            for i, p in enumerate(ps):
+                f.write(point_type+'('+str(i+1)+')'+' = '+str(p).replace('[', '{').replace(']', '}')+';\n')
+
+        line_count = 1
+        for line_type, ls in lines.iteritems():
+            assert line_type in self.line_types
+            for l in ls:
+                f.write(line_type+'('+str(line_count)+')'+' = '+str(l).replace('[', '{').replace(']', '}')+';\n')
+                line_count += 1
+
+        for surface_type, ss in surfaces.iteritems():
+            assert surface_type in self.surface_types
+            for s in ss:
+                f.write(surface_type+'('+str(line_count)+')'+' = '+str(s).replace('[', '{').replace(']', '}')+';\n')
+                line_count += 1
+        del line_count
+
+        for boundary_type, bs in boundary_types.iteritems():
+            for b in bs:
+                if str(boundary_type) == 'boundary':
+                    f.write('Physical Surface'+'("'+str(boundary_type)+'")'+' = '+str(b).replace('[', '{').replace(']', '}')+';\n')
+                else:
+                    f.write('Physical Line'+'("'+str(boundary_type)+'")'+' = '+str(b).replace('[', '{').replace(']', '}')+';\n')
+
+        f.close()
+
+    def __repr__(self):
+        return 'PolygonalDomain'
+
+
+class PieDomain(PolygonalDomain):
+
+    def __init__(self, angle, lc=1.):
+        self.angle = angle
+        from math import pi, cos, sin
+        assert (0 < angle) and (angle < 2*pi)
+
+        if angle <= pi:
+            points = {'Point': [[0., 0., 0, lc], [1., 0., 0, lc], [cos(angle), sin(angle), 0, lc]]}
+            lines = {'Line': [[1, 2], [3, 1]], 'Circle': [[2, 1, 3]], 'Line Loop': [[1, 2, 3]]}
+            surfaces = {'Plane Surface': [[4]]}
+            boundary_types = {BoundaryType('dirichlet'): [[1, 2]], BoundaryType('neumann'): [[3]], 'boundary': [[5]]}
+        else:
+            points = {'Point': [[0., 0., 0, lc], [1., 0., 0, lc], [-1., 0., 0, lc], [cos(angle), sin(angle), 0, lc]]}
+            lines = {'Line': [[1, 2], [4, 1]], 'Circle': [[2, 1, 3], [3, 1, 4]], 'Line Loop': [[1, 2, 3, 4]]}
+            surfaces = {'Plane Surface': [[5]]}
+            boundary_types = {BoundaryType('dirichlet'): [[1, 2]], BoundaryType('neumann'): [[3, 4]], 'boundary': [[6]]}
+        import os.path
+        geo_file_path = os.path.join(os.path.dirname(__file__), '../../../testdata/PieDomain'+str(angle).replace('.', '_')+'.geo')
+
+        super(PieDomain, self).__init__(points, lines, surfaces, boundary_types, geo_file_path)
+
+    def __repr__(self):
+        return 'PieDomain({})'.format(self.angle)
