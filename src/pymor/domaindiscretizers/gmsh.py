cd4fe222f9f7644c1b32b12979ffbbbf5d6966c3
Michael Laier <m_laie01@uni-muenster.de>
2015-10-05 18:18:28 +0200

    [domaindiscretizers.gmsh] add more options to discretize_PolygonalDomain

--- src/pymor/domaindiscretizers/gmsh.py
+++ src/pymor/domaindiscretizers/gmsh.py
@@ -5,13 +5,19 @@ from pymor.playground.grids.gmsh import GmshBoundaryInfo
 from pymor.playground.grids.gmsh import GmshGrid
 
 
-def discretize_PolygonalDomain(domain_description, mesh_algorithm='meshadapt'):
-    assert isinstance(domain_description, PolygonalDomain)
-    geo_file_path = domain_description.geo_file_path
+def discretize_PolygonalDomain(domain_description=None, geo_file=None, mesh_algorithm='meshadapt', clscale=1.,
+                               clmin=0.1, clmax=0.2, options=''):
+    assert domain_description is None or geo_file is None
+    if domain_description is not None:
+        assert isinstance(domain_description, PolygonalDomain)
+        geo_file_path = domain_description.geo_file_path
+    else:
+        geo_file_path = geo_file
     gmsh_file_path = geo_file_path.replace('.geo', '.msh')
 
     from subprocess import PIPE, Popen
-    gmsh = Popen(['gmsh', geo_file_path, '-2', '-algo', mesh_algorithm, '-o', gmsh_file_path], stdout=PIPE)
+    gmsh = Popen(['gmsh', geo_file_path, '-2', '-algo', mesh_algorithm, '-clscale', str(clscale), '-clmin', str(clmin),
+                  '-clmax', str(clmax), options, '-o', gmsh_file_path], stdout=PIPE)
     gmsh.wait()
     print(gmsh.stdout.read())
 
